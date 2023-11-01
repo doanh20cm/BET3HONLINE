b3d30e7c720bdd2c12c747e1f3458e6538f21ae2
doanh20cm <doanhidm1@gmail.com>
2023-11-01 23:34:35 +0700

    add b7

---
+++ TestCS/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+
+    static int Nhap(int index)
+    {
+        int n;
+        Console.Write("nums[{0}] = ", index);
+        while (!int.TryParse(Console.ReadLine(), out n))
+        {
+            Console.Write("Nhap lai nums[{0}] = ", index);
+        }
+        return n;
+    }
+
+    static void Main(string[] args)
+    {
+        var nums = new List<int>();
+        for (int i = 0; i < 10; i++)
+        {
+            nums.Add(Nhap(i));
+        }
+        var result = nums.Where(
+            n => n % 2 == 1 && n > 5)
+            .OrderByDescending(n => n).ToList();
+    }
+}
